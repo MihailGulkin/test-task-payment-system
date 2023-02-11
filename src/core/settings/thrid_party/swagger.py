cfa6e7bff8d359956079695c9ddaa4888bf5d99b
Bol4onok <95026332+MihailGulkin@users.noreply.github.com>
2023-02-11 19:07:18 +0300

    initial commit

---
+++ src/core/settings/thrid_party/swagger.py
@@ -0,0 +1,9 @@
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Web music catalog api',
+    'DESCRIPTION': 'test music catalog task',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    "COMPONENT_SPLIT_REQUEST": True,
+
+    'DISABLE_ERRORS_AND_WARNINGS': True,
+}
